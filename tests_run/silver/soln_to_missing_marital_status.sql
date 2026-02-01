71fa3598e0012e7be887ff159630a7782ef79c4f
Finbar Kizito <emmanuelkizito8@gmail.com>
2026-02-01 20:12:54 +0000

    Update soln_to_missing_marital_status.sql

--- tests_run/silver/soln_to_missing_marital_status.sql
+++ tests_run/silver/soln_to_missing_marital_status.sql
@@ -1,6 +1,6 @@
 
 /**********************************************************
-TITLE: Fixing a Missing Column in the Bronze Layer 
+TITLE: To fix a Missing Column in the Bronze Layer 
 **********************************************************/
 
 ----------------------------------------
